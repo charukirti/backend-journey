eb8211ee93d4793955cf2703b25404995c33de43
charukirti <charukirticc112002@gmail.com>
2025-08-04 18:52:57 +0530

    Day-01: explored fs module for file creation, reading, write, rename, and deletion.

---
+++ Day-01/index.js
@@ -0,0 +1,61 @@
+import fs from 'fs';
+
+// write file
+
+// fs.writeFile('hello.txt', 'Hello, good that you started learning backend with javascript. Happy Coding :)', function (err) {
+//     if (err) console.error(err);
+//     else console.log('Wrote a new/existing file');
+// });
+
+// append file
+
+// fs.appendFile('hello.txt', 'I am really happy to see your progress, keep going. And please dont use mobile whole day. keep it between 2-3hrs. not more than that', function (err) {
+//     if (err) console.error(err);
+
+//     else console.log('File Append done');
+// });
+
+
+// rename file
+
+// fs.rename('hello.txt', 'Hello.txt', function (err) {
+//     if (err) console.error(err);
+//     else console.log('File has been renamed successfully!');
+// });
+
+// copy file
+
+// fs.copyFile('Hello.txt', './copy/HelloCopy.txt', function (err) {
+//     if (err) console.error(err);
+//     else console.log('File copied to specified destination');
+// });
+
+
+// remove file
+
+// fs.unlink('./copy/HelloCopy.txt', function (err) {
+//     if (err) console.error(err.message);
+//     else console.log('File removed, Successfully');
+// });
+
+// remove dir
+
+// fs.rm('./copy', { recursive: true }, function (err) {
+//     if (err) console.error(err.message);
+//     else console.log('Directory removed successfully');
+// });
+
+// create dir
+
+// fs.mkdir('./newFolder', { recursive: true }, function (err) {
+//     if (err) console.error(err.message);
+//     else console.log('New directory created, successfully');
+// });
+
+// read dir
+
+// fs.readdir('./newFolder', function (err, files) {
+//     if (err) console.error(err.message);
+//     else console.log('Files in newFolder dir', files);
+// })
+
