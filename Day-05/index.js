197eb0bbdcbff8cd7d753da753274dcbb2e2a936
charukirti <charukirticc112002@gmail.com>
2025-08-20 11:29:21 +0530

    Learnt all about rest apis

--- Day-05/index.js
+++ Day-05/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const users = require('./mock_data.json');
-
+const fs = require('fs')
 const app = express();
 const PORT = 3000;
+
+app.use(express.urlencoded({extended: false}))
 
 // routes
 app.get('/', (req, res) => {
@@ -16,30 +18,52 @@ app.get('/api/users', (req, res) => {
 app.get('/users', (req, res) => {
     const html = `<ul>
     ${users.map(user => `<li>${user.first_name}</li>`).join('')}
-    </ul>`
+    </ul>`;
 
-    res.send(html)
+    res.send(html);
 });
 
 
 app.get('/api/users/:id', (req, res) => {
-    const id = Number(req.params.id)
+    const id = Number(req.params.id);
 
-    const user = users.find(user => user.id === id)
+    const user = users.find(user => user.id === id);
 
-    return res.json(user)
-})
+    return res.json(user);
+});
 
 app.post('/api/users', (req, res) => {
-    //TODO: CReate new user
-
+    //TODO: Create new user
+    const body = req.body
+    console.log('Body', body)
+    users.push({...body, id: users.length + 1})
+    fs.writeFile("./mock_data.json", JSON.stringify(users), (err, data) => {
+        return res.json({status: 'pending'})
+    })
     return res.json({status: 'pending'})
 })
 
 
-app.patch("/api/users/:id", (req, res) => {
-    
-})
+// app.patch("/api/users/:id", (req, res) => {
+//     // TODO: Edit the user
+// })
+
+// app.delete("/api/users/:id", (req, res) => {
+//     // TODO: Delete the user
+// })
+
+
+/* ------------- Grouping ----------- */
+
+app.route("/api/users/:id").get((req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find(user => user.id === id);
+    return res.json(user);
+}).put((req, res) => {
+    return res.json({ status: 'Pending' });
+}).delete((req, res) => {
+    return res.json({ status: 'Pending' });
+});
 
 // listen
 app.listen(PORT, () => console.log(`Server Started at PORT ${PORT}`));
